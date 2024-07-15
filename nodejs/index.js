3c539b838b7152ac9da807250ff2c24cebe798da
Suja2004 <sujan.22cs164@sode-edu.in>
2024-07-16 04:07:08 +0530

    workshop

---
+++ nodejs/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.use('/myapi',(req,res)=>{
+    res.send("Welcome to my server");
+})
+app.listen(port,()=>{
+    console.log("server listening to 5000")
+})
