1a443fb105e336241d1031b4ed0dd29e1028489b
Sujan Kumar K <143172943+Suja2004@users.noreply.github.com>
2024-07-16 09:57:15 +0530

    Add files via upload

---
+++ jsworkshop/script.js
@@ -0,0 +1,18 @@
+// const a=10
+// var b= 20
+// let c=12.22
+// let d =true
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(typeof(d))
+
+
+const obj ={
+    name:"sujan",
+    age: 19
+}
+console.log(obj)
+console.log(obj.name)
+console.log(obj.age)
